60cfa4377250c9fc86ca7145be2b5bb44610efc7
Pedro Jardim <Pedro200219@outlook.com>
2021-12-12 20:12:29 -0300

    :sparkles: Finalized

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,17 +1,20 @@
-import {
-  useEffect,
-  useRef,
-  useState,
-  useCallback,
-} from 'react';
+import { useEffect, useRef, useState, useCallback } from "react";
+import { IconBaseProps } from "react-icons";
+import { useField } from "@unform/core";
 
-import { useField } from '@unform/core';
+import { Container } from "./styles";
 
-import { Container } from './styles';
+interface InputProps {
+  name: string;
+  icon?: IconBaseProps;
+}
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
-
+const Input = ({
+  name,
+  icon: Icon,
+  ...rest
+}: InputProps & React.InputHTMLAttributes<any>) => {
+  const inputRef = useRef<any>(null);
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
 
@@ -31,13 +34,13 @@ const Input = ({ name, icon: Icon, ...rest }) => {
     registerField({
       name: fieldName,
       ref: inputRef.current,
-      path: 'value',
+      path: "value",
     });
   }, [fieldName, registerField]);
 
   return (
     <Container isFilled={isFilled} isFocused={isFocused}>
-      {Icon && <Icon size={20} />}
+      {/* {Icon && <Icon size={20} />} */}
 
       <input
         onFocus={handleInputFocus}
